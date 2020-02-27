91f7abf6c95d1274a804e27e177cd51049ee290c
Roblu20 <robertoto207@hotmail.com>
2020-02-27 13:18:36 -0600

    se alternen los jugadores

--- src/juegodedados/MejorDe5.java
+++ src/juegodedados/MejorDe5.java
@@ -14,16 +14,22 @@ public class MejorDe5 extends javax.swing.JFrame {
     public MejorDe5() {       
     }
      public MejorDe5(String jugador1, String jugador2) {
-        initComponents();        
+        initComponents();  
         setLocationRelativeTo(null);
         setTitle("Juego de dados");
-        setResizable(false);
+        //setResizable(false);
         lblJugador1.setText(jugador1);
         lblJugador2.setText(jugador2);
         txtResultadoJ1.setVisible(false);
         txtResultadoJ2.setVisible(false);
         txtResultadoJ3.setVisible(false);
         txtResultadoJ4.setVisible(false);
+        lblJugador2.setVisible(false);
+        //Dado1Jug2.setVisible(false);
+        //Dado2Jug2.setVisible(false);
+        //jPanel1.setVisible(false);
+        jButton2.setEnabled(false);
+        
      }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -52,12 +58,15 @@ public class MejorDe5 extends javax.swing.JFrame {
         Turno = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         lblJugador1.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         lblJugador1.setText("Jugador 1");
+        getContentPane().add(lblJugador1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 43, -1, -1));
 
         lblJugador2.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         lblJugador2.setText("Jugador 2");
+        getContentPane().add(lblJugador2, new org.netbeans.lib.awtextra.AbsoluteConstraints(276, 43, -1, -1));
 
         jPanel1.setBackground(new java.awt.Color(204, 204, 255));
 
@@ -84,6 +93,8 @@ public class MejorDe5 extends javax.swing.JFrame {
                 .addContainerGap())
             .addComponent(Dado1Jug2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(276, 64, -1, -1));
 
         jPanel2.setBackground(new java.awt.Color(204, 255, 255));
 
@@ -113,12 +124,15 @@ public class MejorDe5 extends javax.swing.JFrame {
                 .addContainerGap(20, Short.MAX_VALUE))
         );
 
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 64, -1, -1));
+
         jButton1.setText("Inicio Jugador 1");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 158, -1, -1));
 
         jButton2.setText("Inicio Jugador 2");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -126,18 +140,27 @@ public class MejorDe5 extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(276, 153, -1, -1));
+        getContentPane().add(txtResultadoJ1, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 232, 70, -1));
+        getContentPane().add(txtResultadoJ2, new org.netbeans.lib.awtextra.AbsoluteConstraints(88, 232, 70, -1));
+        getContentPane().add(txtResultadoJ3, new org.netbeans.lib.awtextra.AbsoluteConstraints(276, 227, 70, -1));
+        getContentPane().add(txtResultadoJ4, new org.netbeans.lib.awtextra.AbsoluteConstraints(403, 227, 70, -1));
 
+        txtSuma1.setEditable(false);
         txtSuma1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtSuma1ActionPerformed(evt);
             }
         });
+        getContentPane().add(txtSuma1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 200, 70, -1));
 
+        txtSuma2.setEditable(false);
         txtSuma2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtSuma2ActionPerformed(evt);
             }
         });
+        getContentPane().add(txtSuma2, new org.netbeans.lib.awtextra.AbsoluteConstraints(335, 191, 70, -1));
 
         btnReiniciar.setText("Reiniciar");
         btnReiniciar.addActionListener(new java.awt.event.ActionListener() {
@@ -145,121 +168,31 @@ public class MejorDe5 extends javax.swing.JFrame {
                 btnReiniciarActionPerformed(evt);
             }
         });
+        getContentPane().add(btnReiniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 274, -1, -1));
 
+        txtGanadas1.setEditable(false);
         txtGanadas1.setText("0");
         txtGanadas1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtGanadas1ActionPerformed(evt);
             }
         });
+        getContentPane().add(txtGanadas1, new org.netbeans.lib.awtextra.AbsoluteConstraints(121, 34, 71, -1));
 
+        txtGanadas2.setEditable(false);
         txtGanadas2.setText("0");
         txtGanadas2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtGanadas2ActionPerformed(evt);
             }
         });
+        getContentPane().add(txtGanadas2, new org.netbeans.lib.awtextra.AbsoluteConstraints(389, 34, 69, -1));
 
         jLabel1.setText("Turno #");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(205, 6, -1, -1));
 
         Turno.setText("1");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGap(4, 4, 4)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                            .addComponent(txtSuma1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                            .addComponent(jButton1))
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                                .addComponent(lblJugador1)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                .addComponent(txtGanadas1, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                                    .addGap(76, 76, 76)))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGap(28, 28, 28)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addGroup(layout.createSequentialGroup()
-                                            .addComponent(lblJugador2)
-                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(txtGanadas2, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                            .addComponent(txtSuma2, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                            .addComponent(jButton2))))
-                                .addGroup(layout.createSequentialGroup()
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(Turno))))
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(txtResultadoJ1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(txtResultadoJ2, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(118, 118, 118)
-                            .addComponent(txtResultadoJ3, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(txtResultadoJ4, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(btnReiniciar))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(9, 9, 9)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1)
-                            .addComponent(Turno)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(43, 43, 43)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(lblJugador1)
-                                    .addComponent(lblJugador2)))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(txtGanadas2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(txtGanadas1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1)
-                            .addComponent(jButton2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(txtSuma2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(txtResultadoJ3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(txtResultadoJ4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(txtSuma1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(txtResultadoJ1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(txtResultadoJ2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))))
-                .addGap(18, 18, 18)
-                .addComponent(btnReiniciar))
-        );
+        getContentPane().add(Turno, new org.netbeans.lib.awtextra.AbsoluteConstraints(254, 6, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -330,6 +263,18 @@ public class MejorDe5 extends javax.swing.JFrame {
             
             sumaFinal1 = Integer.parseInt(resultado1) + Integer.parseInt(resultado2);
             txtSuma1.setText(sumaFinal1.toString());
+            
+        lblJugador2.setVisible(true);
+        Dado1Jug2.setVisible(true);
+        Dado2Jug2.setVisible(true);
+        jPanel1.setVisible(true);
+        jButton2.setEnabled(true);
+        
+        lblJugador1.setVisible(false);        
+        //Dado1Jug1.setVisible(false);
+        //Dado2Jug1.setVisible(false);
+        //jPanel2.setVisible(false);
+        jButton1.setEnabled(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -431,6 +376,16 @@ public class MejorDe5 extends javax.swing.JFrame {
             if(Integer.parseInt(Turno.getText()) > 5){
                  JOptionPane.showMessageDialog(null, "Fin de la partida", "ERROR", JOptionPane.WARNING_MESSAGE);
             }
+            
+         lblJugador2.setVisible(false);                
+        //jPanel1.setVisible(false);
+        jButton2.setEnabled(false);
+        
+        lblJugador1.setVisible(true);        
+        Dado1Jug1.setVisible(true);
+        Dado2Jug1.setVisible(true);
+        //jPanel2.setVisible(true);
+        jButton1.setEnabled(true);
 
             /*int contadorturno = Integer.parseInt(Turno.getText());        
         String resultado = "";
